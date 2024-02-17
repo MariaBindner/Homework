776bd005015845a11f5c86314c643804919f89da
Maria Bindner <maria.bindner@gmail.com>
2024-02-16 18:54:51 -0600

    MB updated Week 3 homework since Thu night review

--- Week3.js
+++ Week3.js
@@ -1,62 +1,70 @@
-const { serialize } = require("v8");
+//1. Create an array of pizzaToppings with at least four different toppings
+const pizzaToppings = ["mushrooms, ", "pepperoni, ", "sausage, ", "& hamburger "];
 
-//1. Create an array of pizzaToppings with at least four different toppings//
-const pizzaToppings = ["mushrooms", "pepperoni", "sausage", "hamburger"];
+//2. Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c",
 
-//2. Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c",//
-
-//let name = "Maria"
-
-function greetCustomer() {
-  //return (('Hello ${name}!');
-  let greeting = "Welcome, we have four toppings today: ";
-  for (let pizzaTopping of pizzaToppings) {
-    greeting += ` ${pizzaTopping},`;
+function greetCustomer(toppings) {
+  let greeting = "Welcome, to cheezy pizza! We have four toppings today: ";
+  for (let topping of toppings) {
+    greeting += `${topping}`;
   }
   console.log(greeting);
 }
-greetCustomer();
+greetCustomer(pizzaToppings);
 
-// function welcome(name) {
-//     return '${name} welcome, we have the following four delicious toppings today!`;
-
-//     for pizzaToppings (let i=0; 1< pizzaToppings.length; i++) {
-//     console.log(pizzaToppings[i]);
-// }
-
-// 3. Create a getPizzaOrder function that//
-// has the parameters size, crust, and an indefinite amount of toppings as inputs//
-// prints the order, i.e. "One large thick crust pizza with x, y, z, ... coming up!"//
-// outputs a list with the size, crust, and toppings//
+// 3. Create a getPizzaOrder function that
+// has the parameters size, crust, and an indefinite amount of toppings as inputs
+// prints the order, i.e. "One large thick crust pizza with x, y, z, ... coming up!"
+// outputs a list with the size, crust, and toppings
 
 function getPizzaOrder(size, crust, ...toppings) {
-  //   return "One large thick crust pizza with [pizzaToppings]";
+  let list = "";
+
+  for(let topping of toppings) {
+    list += `${topping}, `;
+  }
+
+  let output = `One ${size} ${crust} crust pizza with ${list} ... coming up!`;
+
+  console.log(output);
 }
-// console.log(getPizzaOrder);
+getPizzaOrder("small", "thin", "mushrooms", "sausage");
+
+ //return [size, crust, toppings];
 //4. Create a preparePizza function that
 //has an array as its parameter with three items: a size, a crust, and a list of toppings
 //prints something like "...Cooking pizza..."
-//outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings//
+//outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings
 
-// function preparePizza () {
+function preparePizza(pizza) {
+console.log("...Cooking pizza...");
 
-// }
-// const preparePizza = {
-//     size: "large"
-//     crust: "thin"
-//     toppings: [
-//     "Mushroom"
-//     "Sausage"
-//     "Sausage"
-// ]
-// }
+return {
+    size: pizza[0],
+    crust: pizza[1],
+    toppings: pizza[2]
+}
+}
+preparePizza(["medium", "thin", ["hamburger", "sausage"]]);
+
 //5. Create a servePizza function that
 // has a parameter of a pizza Object
 // logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!"
-// outputs the same pizza Object that was passed in//
-
-// function servePizza() {
+// outputs the same pizza Object that was passed in
 //   return "Order up! Here's your large thick crust pizza with [pizzaToppings], Enjoy!";
-// }
 
-//Call each function and (starting with preparePizza) use the returned value from the previous function as its input//
+function servePizza(pizza) {
+let list = "";
+for(let topping of pizza.toppings) {
+  list += `${topping}, `;
+}
+let output = `Order up! Here's your ${pizza.size} ${pizza.crust} crust pizza with ${list}... Enjoy!';
+
+console.log(output);
+
+return pizza;
+}
+
+
+//Call each function and (starting with preparePizza) use the returned value from the previous function as its input
+servePizza(preparePizza(getPizzaOrder("medium", "thin", "hamburger", "mushroom"));)
